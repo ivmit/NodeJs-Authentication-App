0bd41e07cc8cb014714f2e79fdb011f7763fe526
imitrach <ionut.mitrache@misys.com>
2016-09-06 17:09:00 +0300

    add fb communication

--- config/auth.js
+++ config/auth.js
@@ -1,3 +1,17 @@
 /**
  * Created by imitrach on 9/5/2016.
  */
+module.exports = {
+
+    'facebookAuth': {
+        'clientID'      : '401218400048628',
+        'clientSecret'  : '7eeec00b44883fcc537be48053063673',
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback/'
+    },
+    'twitterkAuth': {
+        'consumerKey'    : 'ub4U9DkIlgwvSEYCK4s5LvdU1',
+        'consumerSecret' : 'cInwn079mRcZwsfeQc3TjQOrUNVlOnAYbXm0jlAdGsrKwOEW2d',
+        'callbackURL'    : 'http://127.0.0.1:8080/auth/twitter/callback'
+    }
+
+};
--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,12 @@
  * Created by imitrach on 9/5/2016.
  */
 var LocalStrategy   = require('passport-local').Strategy;
+var FacebookStragey = require('passport-facebook').Strategy;
+var TwitterStrategy = require('passport-twitter').Strategy;
+
 var User            = require('../models/user');
+
+var configAuth = require('./auth');
 
 module.exports = function(passport){
 
@@ -87,6 +92,77 @@ module.exports = function(passport){
 
         });
     }));
+
+
+//FACEBOOK=========================
+    passport.use(new FacebookStragey({
+
+        //configure
+        clientId    : configAuth.facebookAuth.appID,
+        clientSecret: configAuth.facebookAuth.clientSecret,
+        callbackUrl : configAuth.facebookAuth.callbackURL
+
+    },
+    //facebook will send back the token and profile
+     function(token, refreshToken, profile, done) {
+
+         //async
+         process.nextTick(function(){
+
+
+             //find the user in db based on fb id
+             User.findOne({'facebook.id' : profile.id}, function(err, user){
+
+                 if(err) return done(err);
+
+                 if(user) {
+                     //case user found
+                     return done(null, user)
+                 } else {
+                     //case user not found
+                     var newUser    = new User();
+
+                     //set fb info in user model
+                     newUser.facebook.id    = profile.id; //set user fb id
+                     newUser.facebook.token = token; // save token provided by fb
+                     newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName; //check passport user profile to see how names are returned
+                     newUser.facebook.email = profile.emails[0].value; //facebook can return multiple emails so we'll take the first
+
+
+                     //save our user to the database
+                     newUser.save(function(err){
+                         if (err) throw err;
+
+                         return done(null, newUser);
+
+                     });
+                 }
+             });
+
+         });
+
+     }));
+
+    /*passport.use(new TwitterStrategy({
+        consumerKey   : configAuth.twitterkAuth.consumerKey,
+        consumerSecret: configAuth.twitterkAuth.consumerSecret,
+        callbackUrl   : configAuth.twitterkAuth.callbackURL
+    },
+    function(token, tokenSecret, profile, done) {
+
+        //make code async
+        //User.findOne won't fire until we have all our data from tw
+
+        process.nextTick(function(){
+            User.findOne({'twitter.id' : profile.id }, function(err, user){
+
+            })
+        })
+
+    })*/
+
+
+
 
 };
 
